15f8f529cb0a89c30548ca784db47d494b53275e
vivekaps16 <vivekaps16@gmail.com>
2024-11-12 17:07:25 +0530

    vault tokn domain

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "http://vault-internal.rdevopsb81.online:8200"
+  address         = "http://vault-internal.devopspractice.info:8200"
   token           = var.vault_token
   skip_tls_verify = true
 }
